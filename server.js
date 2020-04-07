06cc4aec67e8715980b80e1544d2e09255d7fbae
Eduardo Romanini <eduroma@globo.com>
2020-04-07 16:35:10 -0300

    Set up a Template Engine

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const express = require("express");
+const pug = require("pug");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.set("view engine", "pug");
+
+app.route("/").get((req, res) => {
+  // res.sendFile(process.cwd() + "/views/index.html");
+  res.render(process.cwd() + "/views/pug/index");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
