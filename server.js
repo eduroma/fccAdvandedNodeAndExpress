c04ed9ac94b65b8991a70bf436611ea4fbc7f345
Eduardo Romanini <eduroma@globo.com>
2020-04-07 17:24:41 -0300

    Use a Template Engine's Powers

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.set("view engine", "pug");
 
 app.route("/").get((req, res) => {
   // res.sendFile(process.cwd() + "/views/index.html");
-  res.render(process.cwd() + "/views/pug/index");
+  res.render(process.cwd() + "/views/pug/index", {
+    title: "Hello",
+    message: "Please login",
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
