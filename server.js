5a73707b8783cd6a5724c57a5de459ed2174d490
Eduardo Romanini <eduroma@globo.com>
2020-04-09 14:36:26 -0300

    How to Put a Profile Together

--- server.js
+++ server.js
@@ -58,7 +58,9 @@ function ensureAuthenticated(req, res, next) {
 }
 
 app.route("/profile").get(ensureAuthenticated, (req, res) => {
-  res.render(process.cwd() + "/views/pug/profile");
+  res.render(process.cwd() + "/views/pug/profile", {
+    username: req.user.username,
+  });
 });
 
 mongo.connect(process.env.DATABASE, (err, cluster) => {
